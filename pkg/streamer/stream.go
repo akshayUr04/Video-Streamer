79c72e8d3a3a157b442410dda184f8e21239c109
akshayUr04 <akshayur0404@gmail.com>
2023-07-20 01:06:56 +0530

    updated

--- pkg/streamer/stream.go
+++ pkg/streamer/stream.go
@@ -2,9 +2,8 @@ package streamer
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"time"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,48 +13,26 @@ func Streamer(c *gin.Context) {
 	videoID := c.Param("id")
 	playlist := c.Param("playlist")
 
-	// Create a channel to receive the playlist data
-	playlistDataChan := make(chan []byte)
-	errChan := make(chan error)
-
-	go func() {
-		// Fetch the playlist data in a separate goroutine
-		playlistData, err := readPlaylistData(videoID, playlist)
-		if err != nil {
-			errChan <- err // Send the error through the error channel
-			return
-		}
-		playlistDataChan <- playlistData // Send the playlist data through the data channel
-	}()
-
-	select { //select helps to perform non-blocking operation on channels. It waits until one case is ready to proceed, and if multiple cases are ready to proceed, it randomly selects one to execure.
-	case playlistData := <-playlistDataChan: // if data is available on the playlistDataChan channel
-		// Set the response headers
-		c.Header("Content-Type", "application/vnd.apple.mpegurl")
-		c.Header("Content-Disposition", "inline")
-
-		// Write the playlist data to the response body
-		c.Writer.Write(playlistData)
-	case err := <-errChan: // This case checks if there is an error available on the errChan channel
-		// Handle the error
+	playlistData, err := readPlaylistData(videoID, playlist)
+	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "failed to read file from server",
 			"error":   err.Error(),
 		})
-	case <-time.After(5 * time.Second): //This case uses the time.After function to create a channel that sends a value after a specified duration.
-		// Handle the timeout case
-		c.JSON(http.StatusGatewayTimeout, gin.H{
-			"message": "request timed out",
-		})
+		return
 	}
+
+	c.Header("Content-Type", "application/vnd.apple.mpegurl") //inform the client that the content type is a hls play list
+	c.Header("Content-Disposition", "inline")                 //specify how the client needs to handle the repsonce content
+	c.Writer.Write(playlistData)                              //writes the HLS playlist data to the HTTP response body
 }
 
 func readPlaylistData(videoID, playlist string) ([]byte, error) {
 	// Construct the playlist file path
 	playlistPath := fmt.Sprintf("storage/%s/%s", videoID, playlist)
-
+	fmt.Println("---------------read file-------------------")
 	// Read the playlist file
-	playlistData, err := ioutil.ReadFile(playlistPath)
+	playlistData, err := os.ReadFile(playlistPath)
 	if err != nil {
 		fmt.Println(err, err.Error())
 		return nil, err
